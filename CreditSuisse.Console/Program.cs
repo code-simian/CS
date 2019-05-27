1104f519cbf78ad5e632d968746e8b946f2d513a
stefan.chirita <stefan.chirita.07@gmail.com>
2019-05-27 20:21:37 +0100

    gitignore and hello world skeleton

---
+++ CreditSuisse.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditSuisse.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
